5fce5fcdee57c18a185298474052f547c7dc3ff4
jpthibault20 <tjeanpierre757@gmail.com>
2023-12-15 09:56:35 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h3>Bonjour le monde</h3>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
