6e761b25eb6691fd652d23a0c520ef990da673e2
thibault <tjeanpierre757@gmail.com>
2023-12-18 14:27:47 +0100

    P2C3 exervice

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,18 +1,29 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
+
+    const category = plantList.reduce((acc, valCourant) => acc.includes(valCourant.category) ? acc : acc.concat(valCourant.category), [])
     return (
+    <div>
         <ul>
-            {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{ plant }</li>
-            ))}
+            {
+                category.map((cat) => (
+                    <li key={`${cat}`}>
+                        {cat}
+                    </li>
+                ))
+            }
         </ul>
+        <ul>
+            {
+                plantList.map((plant, i) => (
+                    <li key={`${plant.name}-${i}`}>
+                        {plant.name}
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
     )
 }
 
